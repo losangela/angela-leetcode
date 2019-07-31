fe4f8aa809969edcbfe062c6b2e750ca90817d45
losangela <46168805+losangela@users.noreply.github.com>
2019-07-30 20:20:50 -0700

    Update numSubarrayProductLessThanK.js

--- numSubarrayProductLessThanK/numSubarrayProductLessThanK.js
+++ numSubarrayProductLessThanK/numSubarrayProductLessThanK.js
@@ -1,11 +1,12 @@
-/* 
-Your are given an array of positive integers nums.
-
+/*
+========================================
+================PROMPT==================
+========================================
+You are given an array of positive integers nums.
 Count and print the number of (contiguous) subarrays where the product of all the
 elements in the subarray is less than k.
 
 Example 1:
-
 Input: nums = [10, 5, 2, 6], k = 100
 Output: 8
 Explanation: The 8 subarrays that have product less than 100 are:
@@ -14,13 +15,10 @@ Note that [10, 5, 2] is not included as the product of 100 is not strictly less 
 
 
 Note:
-
 0 < nums.length <= 50000.
 0 < nums[i] < 1000.
 0 <= k < 10^6.
 */
-
-
 
 const numSubarrayProductLessThanK = (nums, k) => {
   let left = 0;
@@ -40,6 +38,9 @@ const numSubarrayProductLessThanK = (nums, k) => {
 console.log(numSubarrayProductLessThanK([10, 5, 2, 6], 100), `should equal to 8`)
 
 /*
+========================================
+================SOLUTION================
+========================================
 Input: nums = [10, 5, 2, 6], k = 100
 Output: 8
 
