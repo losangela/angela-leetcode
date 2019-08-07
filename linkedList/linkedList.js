c163959c8f91704ec73a8cbed30a0b4e3684dea2
Angela Choi <eunsol.angela.choi@gmail.com>
2019-08-07 14:12:39 -0700

    linkedlist

---
+++ linkedList/linkedList.js
@@ -0,0 +1,18 @@
+/* 
+linked list exercise
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
