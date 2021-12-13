a31e2fc587daadf43444b41c01aa2117be08f637
losangela <eunsol.angela.choi@gmail.com>
2021-12-12 23:27:37 -0800

    added project euler problems

---
+++ ProjectEuler/problem03/index.js
@@ -0,0 +1,26 @@
+/*
+
+Largest prime factor
+Problem 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+*/
+
+
+const numberIsPrime = (n) => { // must be greater than 2
+  let isPrime = true;
+  let i = 2;
+  while (i < n) {
+    if (n % i === 0) {
+      isPrime = false
+      break;
+    }
+    i++
+  }
+  return isPrime
+}
+
+console.log(numberIsPrime(2))
