ef235a7965072bd22b53e1c414c7aaff2712f595
losangela <eunsol.angela.choi@gmail.com>
2021-12-17 12:41:21 -0800

    working on 8

---
+++ ProjectEuler/problem07/index.js
@@ -0,0 +1,28 @@
+/*
+
+10001st prime
+Problem 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+*/
+
+import { numberIsPrime } from "../problem03/index.js";
+
+const findNthPrimeNumber = (n) => {
+  const primes = [];
+  let i = 2;
+  
+  while (primes.length < n) {
+    if (numberIsPrime(i)) {
+      primes.push(i);
+    }
+    i++
+  }
+  return primes[n - 1]
+}
+
+
+console.log(findNthPrimeNumber(10001))
