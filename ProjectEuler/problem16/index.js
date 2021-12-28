1d23aa913f00ac8ee146fd454a92d268bf80a4d6
losangela <eunsol.angela.choi@gmail.com>
2021-12-28 13:13:04 -0800

    completed prob 16 with BigInt

---
+++ ProjectEuler/problem16/index.js
@@ -0,0 +1,21 @@
+/*
+
+Power digit sum
+Problem 16
+
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+*/
+
+const sumOfDigitsOfN = (n) => {
+  let bigNToString = BigInt(n).toString();
+  let sum = 0;
+  for (let i = 0; i < bigNToString.length; i++) {
+    sum += parseInt(bigNToString[i], 10)
+  }
+  return sum
+}
+
+console.log(sumOfDigitsOfN(Math.pow(2, 1000)))
