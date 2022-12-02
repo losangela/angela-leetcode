76373cac4346086fa061b05bef25da31d0001c9b
losangela <eunsol.angela.choi@gmail.com>
2022-12-02 13:51:29 -0800

    part 2

--- AdventOfCode/2022/day1.js
+++ AdventOfCode/2022/day1.js
@@ -4,9 +4,20 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   const elves = [];
   const calories = data.split('\n').map(x => parseInt(x));
   const topCalories = [];
-  
+
   let mostCalories = 0;
   let currentElfCalories = 0;
+
+  const addTopCalories = (newCalories) => {
+    if (topCalories[2] < newCalories) {
+      topCalories.pop();
+    }
+
+    if (topCalories.length < 3) {
+      topCalories.push(newCalories);
+      topCalories.sort((a, b) => b - a);
+    }
+  }
 
   calories.map(cal => {
     if (!cal) {
@@ -15,6 +26,7 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
         elf: elves.length + 1,
         calories: currentElfCalories,
       })
+      addTopCalories(currentElfCalories);
       currentElfCalories = 0;
       return;
     }
@@ -22,11 +34,14 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   })
 
   if (currentElfCalories) {
+    mostCalories = Math.max(mostCalories, currentElfCalories);
     elves.push({
       elf: elves.length + 1,
       calories: currentElfCalories,
     })
+    addTopCalories(currentElfCalories);
     currentElfCalories = 0;
   }
-  console.log({ elves, mostCalories })
+  console.log({ part: 1, elves, mostCalories })
+  console.log({ part: 2, topCalories, sum: topCalories.reduce((a, b) => a + b) })
 })
---
+++ AdventOfCode/2022/day2.js
@@ -0,0 +1,4 @@
+import fs from 'fs';
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+})
